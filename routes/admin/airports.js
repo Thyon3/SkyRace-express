eeb6768fe46eec7ee12cac238e31cbaa34c2d165
Thyon3 <asnakemengesha80@gmail.com>
2025-12-30 09:04:33 -0500

    Backend: Add Airport routes

---
+++ routes/admin/airports.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const airportController = require('../../controllers/admin/airportController');
+const { requireAdmin } = require('../../middleware/adminAuth');
+
+router.use(requireAdmin);
+
+router.get('/', airportController.getAllAirports);
+router.post('/', airportController.createAirport);
+router.patch('/:id', airportController.updateAirport);
+router.delete('/:id', airportController.deleteAirport);
+
+module.exports = router;
