c1baf61922a8e2e872290b06510dba7876da95fb
Thyon3 <asnakemengesha80@gmail.com>
2025-12-31 06:33:49 -0500

    docs: add Swagger configuration for API documentation

---
+++ utils/swagger.js
@@ -0,0 +1,37 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'SkyRace API',
+            version: '1.0.0',
+            description: 'API documentation for SkyRace flight booking application',
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000',
+                description: 'Development server',
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+    apis: ['./routes/*.js', './routes/admin/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;
